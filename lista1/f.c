d988000ed3b097d4aff66211842105cbe8df4f9e
GZaranza <gabrielzaranza@hotmail.com>
2023-10-07 23:28:01 -0300

    a

--- lista1/f.c
+++ lista1/f.c
@@ -1,51 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
-typedef struct {
-    int x;
-    int y;
-} tiro;
+int c;
+int t;
+long long int circulos[100010];
 
-int distancia_ponto(int x, int y){
-    int soma = (x*x)+(y*y);
-    return soma;
+int distancia(int x, int y){
+    
+    int dist = (x*x) + (y*y);
+    return dist;
+
 }
+
+int busca(long long int x){
+    int i=1;
+    int j=c;
+
+    if(x>circulos[c]) return 0;
+
+    while (i<j){
+        int meio = (i+j)/2;
+
+        if(circulos[meio]>=x){
+            j=meio;
+        }
+        else{
+            i=meio+1;
+        }
+
+    }
+    return c+1-j;
+
+}
+
 
 int main(){
 
-    int c,t;
-    int pontuacao =0;
+    long long int pontuacao=0;
 
+    scanf("%d %d", &c, &t);
 
-    scanf("%d %d", &c,&t);
-
-    int raios[c];
-    int aux;
     
 
-    for(int i=0;i<c;i++){
-        scanf("%d", &aux);
-        raios[i]=aux*aux;
-        //printf("%d\n", raios[i]);
+    for(int i=1; i<=c;i++){
+        scanf("%lld", &circulos[i]);
+        circulos[i]=circulos[i]*circulos[i];
     }
+    
+    long long int x=0,y=0;
 
-    //tiro tentativas[t] ;
-    int x,y;
-    int dist[t];
-
-    for(int i=0;i<t;i++){
-        scanf("%d %d", &x, &y);
-        dist[i]=distancia_ponto(x,y);
-        //printf("%d\n", dist[i]);
+    for(int i=1; i<=t;i++){
+        scanf("%lld %lld", &x,&y);
+        pontuacao += busca(x*x+y*y);
     }
-   
-
-    for(int i=0;i<t;i++){
-        for(int i=0;i<t;i++){
-            
-        }
-    }
-
-    printf("%d\n", pontuacao);
+    printf("%lld\n", pontuacao);
+    return 0;
 }
